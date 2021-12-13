298d0d8b7f3df2a8aec2ac5e2bae5a226339df12
Pieter Wenji <pieterjake321@gmail.com>
2021-12-13 11:43:24 +0700

    fix string 2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function requireHTTPS(req, res, next) {
-  if (!req.secure && req.get("x-fowarded-proto") !== "https") {
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
     return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
